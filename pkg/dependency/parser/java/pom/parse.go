3741c501b17fa1490a30b2c4c385cd97d7c3a090
Ali Yachnes <ali@arnica.io>
2026-05-15 11:28:47 -0400

    Revert "Remove redundant default (needs testing)"
    This reverts commit f72a8b68ec94c7cd35ebe2dbb3f9270d50a0645b.

--- pkg/dependency/parser/java/pom/parse.go
+++ pkg/dependency/parser/java/pom/parse.go
@@ -78,6 +78,12 @@ type Parser struct {
 	servers             []Server
 }
 
+const (
+	centralURL = "https://repo.maven.apache.org/maven2/"
+)
+
+var mavenReleaseRepos = []string{centralURL}
+
 func NewParser(filePath string, opts ...option) *Parser {
 	var logger = log.WithPrefix("pom")
 
@@ -85,13 +91,14 @@ func NewParser(filePath string, opts ...option) *Parser {
 		offline:            false,
 		useMavenCache:      false,
 		mavenCacheTtl:      720,
+		releaseRemoteRepos: mavenReleaseRepos, // Maven doesn't use central repository for snapshot dependencies
 	}
+
+	logger.Debug("Creating parser", log.String("releaseRemoteRepos", strings.Join(o.releaseRemoteRepos, ", ")))
 
 	for _, opt := range opts {
 		opt(o)
 	}
-
-	logger.Debug("Creating parser", log.String("releaseRemoteRepos", strings.Join(o.releaseRemoteRepos, ", ")))
 
 	s := readSettings()
 	localRepository := s.LocalRepository
